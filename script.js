ac0608d4a05055e362425e30ef1b984e67da081b
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-04 16:13:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-function volume_sphere(e) {
-    //Write your code here
-	e.preventDefault();
+// function volume_sphere(e) {
+//     //Write your code here
+// 	e.preventDefault();
 	
-  const radiusField = document.querySelector("#radius");
-	const vlmField = document.querySelector("#volume");
+//   const radiusField = document.querySelector("#radius");
+// 	const vlmField = document.querySelector("#volume");
 
-	let radius = parseFloat(radiusField.value.trim());
+// 	let radius = parseFloat(radiusField.value.trim());
+//     if(isNaN(radius) }|| radius < 0){
+// 	vlmField.value = "NaN"
+// 	return;
+//  }
+// 	const volume = 4/3 * 3.14 * radius**3;
 
-	if(radius <= 0){
-		vlmField.textContent = "NaN"
-		return;
-}
-	const volume = 4/3 * 3.14 * radius**3;
+// 	vlmField.value = `${volume}`;
+// } 
 
-	vlmField.value = `${volume}`;
-} 
-
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+// window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
