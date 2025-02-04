45348adcf27c49c34fc07d6da60bde5ee72c9c84
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-04 16:05:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,14 +7,13 @@ function volume_sphere(e) {
 
 	let radius = parseFloat(radiusField.value.trim());
 
-	
-	const volume = 4/3 * 3.14 * radius**3;
-
-	vlmField.value = `${volume}`;
 	if(radius <= 0){
 		vlmField.textContent = "NaN"
 		return;
 }
+	const volume = 4/3 * 3.14 * radius**3;
+
+	vlmField.value = `${volume}`;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
